1875460890a201a3c357297a1bbb464548d4d994
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-08-08 09:33:42 -0400

    Initial commit

---
+++ add.py
@@ -0,0 +1,11 @@
+# User enters first number
+a = int(input("Enter the first number: "))
+
+# User enters second number
+b= int(input("Enter second number: "))
+
+# add two numbers together
+c= a + b
+
+# Print value of two variables
+print("the sum of", a, "and", b, "is:", c)
